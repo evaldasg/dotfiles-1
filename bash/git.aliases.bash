3313dc0816d23e0701c36cb02d81e468f3c18983
Clay Bridges <cbridges@litl.com>
2012-06-05 02:35:07 -0400

    Move in git aliases, and initial bashrc

---
+++ bash/git.aliases.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Aliases
+alias g='git'
+alias gs='git status'
+alias gc='git checkout'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gcm='git checkout master'
+alias gmp='git checkout master; git pull origin'
+alias gpo='git pull origin'
+alias gb='git branch'
+alias gba='git branch -a'
+alias gdel='git branch -D'
+alias gmu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
+alias grc='git rebase --continue'
+alias gm='git mergetool'
+alias gl='git log'
+alias gl1='git log --pretty=format:"%h %ad | %s%d [%an]" --date=short'
+
---
+++ bashrc
@@ -0,0 +1,4 @@
+export CLASSPATH=~/.clojure/nailgun/nailgun-0.7.1.jar:$CLASSPATH
+export PATH=~/bin:~/script:$PATH
+
+. ~/.config/bash/git.aliases.bash
